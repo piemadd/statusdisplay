d760cdd42169ff0c0a791b332ad8e6b0d211042e
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-07-31 22:08:11 -0400

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,69 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>Weather:</h1>
+      </main>
+      <main id="weather">
+        <section id="hourly" className={"outerCard"}>
+          <h2>Hourly</h2>
+          <div className={"innerCard foreCast"}>
+            <span>
+              <p>startTime</p>
+              <p> | </p>
+              <span className={"material-symbols-rounded icon"}>windDirection</span>
+              <p>windSpeed</p>
+            </span>
+            <span>
+              <h3>
+                temperature°temperatureUnit shortForecast
+              </h3>
+            </span>
+          </div>
+        </section>
+        <section id="daily" className={"outerCard"}>
+          <h2>Daily</h2>
+        </section>
+      </main>
+      <main>
+        <h1>Transit:</h1>
+      </main>
+      <main id="transit">
+        <section className={"outerCard station"}>
+          <h2>stationName</h2>
+          <div className={"innerCard tripCard"}>
+            <span>
+              <p>
+                lineName runNum to
+              </p>
+            </span>
+            <span>
+              <h3>runDest</h3>
+              <p></p>
+              <b>arrEst</b>
+            </span>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
---
+++ start.sh
@@ -0,0 +1,5 @@
+curl https://bun.sh/install | bash
+export BUN_INSTALL="/home/piero/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+bun dev
